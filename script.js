4ec7c55d3bbd34aa8b5aa892fc535856548824d6
Kagekiiyo <caio.medina79@hotmail.com>
2025-04-20 17:58:43 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btnPresent').addEventListener('click', function() {
-  document.getElementById('btnPresent').style.display = 'none';
-  document.getElementById('msgParabens').classList.remove('hidden');
-});
